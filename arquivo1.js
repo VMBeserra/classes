fa33a6e6a900438e049752befb67c96634dbbea6
VMBeserra <101059618+VMBeserra@users.noreply.github.com>
2022-03-06 15:14:24 -0300

    Meu primeiro arquivo javascript

---
+++ arquivo1.js
@@ -0,0 +1,67 @@
+var myvar
+undefined
+myvar=10
+10
+var soma=15+8
+undefined
+soma
+23
+myvar
+10
+soma=soma*3
+69
+soma*=3
+207
+var souninja=true
+undefined
+var comida[
+arroz,feijao,ovo]
+VM727:1 Uncaught SyntaxError: Unexpected token '['
+var comida= ["arroz" ,"feijao","ovo"]
+undefined
+comida [1]
+'feijao'
+comida.length
+3
+1==1
+true
+1===1
+true
+1=="1"
+true
+1==="1"
+false
+soma===myvar
+false
+myvar<=soma
+true
+var nome=joao
+VM1119:1 Uncaught ReferenceError: joao is not defined
+    at <anonymous>:1:10
+(anonymous) @ VM1119:1
+var nome="joao"
+undefined
+var telefone=254178
+undefined
+var texto= "O telefone de "+nome+" é "+telefone
+undefined
+texto
+'O telefone de joao é 254178'
+nome="Maria"
+'Maria'
+texto
+'O telefone de joao é 254178'
+var texto= "O telefone de "+nome+" é "+telefone
+undefined
+texto
+'O telefone de Maria é 254178'
+var cpf= 231456987
+undefined
+texto= "O cpf de "+nome+"é" +cpf+ "e o telefone é"+telefone
+'O cpf de Mariaé231456987e o telefone é254178'
+texto= "O cpf de "+nome+"é" +cpf+ "e o telefone é "+telefone
+'O cpf de Mariaé231456987e o telefone é 254178'
+var n1
+undefined
+66
+66
